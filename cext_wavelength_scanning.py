246b89de33a59f71f295e69eb22eec3f905b1a2e
Natalia C. Clementi <natyclementi@gmail.com>
2017-02-14 17:17:37 -0500

    Add cext_wave_scanning function to script

--- cext_wavelength_scanning.py
+++ cext_wavelength_scanning.py
@@ -1,4 +1,35 @@
 import numpy
+import pygbe 
+from pygbe.main import main
+
+def Cext_wave_scan(wave_diel, field, example_folder_path):
+
+    '''Computes the extinction cross section using PyGBe for different 
+       wavelength and associated dielectric constants. 
+
+    Arguments:
+    ----------
+    wave_diel          : list, each element is a tuple (wavelength, field('E'))
+                         where field('E') is a list of the dielectric constant 
+                         of each region.       
+    field              : dictionary, config dictionary.
+    example_folder_path: str, path to the example folder relative to wherever
+                         the interpreter was started. 
+
+    Returns:
+    --------  
+    Cext_wave          : list, list of cross extinction sections. 
+    
+    '''
+
+    Cext_wave = []
+    for wave, E in wave_diel:
+        field['E'] = E  
+        results = main(['', example_folder_path], field=field,
+                  lspr=(-1,wave), return_results_dict=True)
+        Cext_wave.append(results['Cext'])
+
+    return Cext_wave
 
 
 def Cext_analytical(radius, wavelength, diel_in, diel_out):
