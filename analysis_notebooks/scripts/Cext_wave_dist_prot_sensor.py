e320b00c725b8a7a3f137718f5027a4f5f7ae144
Naty Clementi <natyclementi@gmail.com>
2018-01-03 15:34:00 -0500

    Finish adapting script for sensor prot runs

--- analysis_notebooks/scripts/Cext_wave_dist_prot_sensor.py
+++ analysis_notebooks/scripts/Cext_wave_dist_prot_sensor.py
@@ -37,47 +37,43 @@ e_p = er_p + 1j*ei_p #protein
 #Building E field for single sphere dictionary
 E_field_single = [list(eps) for eps in zip(e_w, e_s)]
 
-field_dict_single = read_fields('../../../pygbe_dev/pygbe/examples/lspr/sphere_complex.config')
+field_dict_single = read_fields('../../../pygbe_dev/pygbe/examples/BSA_sensor_d=infty/sph_sensor.config')
 
 tic_single = time.time() 
 
 wave_single, Cext_single = Cext_wave_scan(wavelength, E_field_single, field_dict_single,
-                     '../../../pygbe_dev/pygbe/examples/lspr')
+                     '../../../pygbe_dev/pygbe/examples/BSA_sensor_d=infty')
 toc_single = time.time()
 
-numpy.savetxt('../../data/wave_cext_d/wave_cext_d_infty.txt', 
+numpy.savetxt('../../data/wave_cext_d_prot_sensor/wave_cext_d_infty.txt', 
               list(zip(wave_single, Cext_single)),
-              fmt = '%.3f %.8f', 
+              fmt = '%.1f %.8f', 
               header = 'lambda [nm], Cext, d=infty')
 
 
 #Building E field for dictionary (protein)
-e_list = [list(eps) for eps in zip(e_w, e_s, e_p)]
-
-E_field = []
-for lst in e_list:
-    E_field.append(lst+[lst[-1]]*5) 
+E_field = [list(eps) for eps in zip(e_w, e_s, e_p)]
 
 
-distance_path_folders = ['multiple_sphere_d=1', 
-						 'multiple_sphere_d=2',
-						 'multiple_sphere_d=4']
+distance_path_folders = ['BSA_sensor_d=1', 
+						 'BSA_sensor_d=2',
+						 'BSA_sensor_d=4']
 
 tic_d = time.time()
 for path in distance_path_folders:
 
     field_dict = read_fields('../../../pygbe_dev/pygbe/examples/'
-                            +path+'/seven_sphere_complex.config')
+                            +path+'/sphere_bsa.config')
     wave, Cext = Cext_wave_scan(wavelength, E_field, field_dict,
                      '../../../pygbe_dev/pygbe/examples/'+path)
     toc=time.time()
 
-    numpy.savetxt('../../data/wave_cext_d/'+path+'.txt', 
+    numpy.savetxt('../../data/wave_cext_d_prot_sensor/'+path+'.txt', 
               list(zip(wave, Cext)),
-              fmt = '%.3f %.8f', 
+              fmt = '%.1f %.8f', 
               header = 'lambda [nm], Cext'+path)
 toc_d = time.time()
 
-with open('../../data/wave_cext_d/time_wave_Cext_d.txt', 'w') as f:
+with open('../../data/wave_cext_d_prot_sensor/time_wave_Cext_d.txt', 'w') as f:
         print('total run time: {}'.format((toc_single-tic_single)+(toc_d-tic_d)),
                                     file=f)
