bc378578cfd754e07ff3810adc2a76f34acff596
Naty Clementi <natyclementi@gmail.com>
2018-05-14 17:53:50 -0400

    Update on script to plot images

--- paper/BSA_silver_NP_LSPR_response/visualization_images.py
+++ paper/BSA_silver_NP_LSPR_response/visualization_images.py
@@ -4,8 +4,10 @@ locations respect to the sensor.
 
 import numpy
 from matplotlib import pyplot, rcParams
+from mpl_toolkits.mplot3d import Axes3D
 
-def read_data_plot(sensor, prt_one, prt_two, elev, azim, 
+
+def read_data_plot(sensor, prt_one, prt_two, elev, azim, prot_color,
                     file_name=None, file_ext=None):
     '''Reads the mesh files (.vert and .face) necessary to plot the 
     sensor-proteins display and save them as png. 
@@ -43,3 +45,38 @@ def read_data_plot(sensor, prt_one, prt_two, elev, azim,
 
     face_prt2 = numpy.array(list(zip(f_12, f_22, f_32)))
     face_protein_2 = face_prt2[:][:] - 1
+
+    ### Plot image ###
+    
+    fig = pyplot.figure(figsize=(12,12))
+    ax = fig.gca(projection='3d')
+
+    rcParams['font.family'] = 'serif'
+    rcParams['font.size'] = 16
+    rcParams['xtick.top'] = True
+    rcParams['ytick.right'] = True
+    rcParams['axes.linewidth'] = 2
+
+    ax.plot_trisurf(xs, ys, zs, triangles=face_sensor, linewidth=0.2,
+                     edgecolor="black", color="white", alpha=0.3)
+    ax.plot_trisurf(xp1, yp1, zp1, triangles=face_protein_1, linewidth=0.2,
+                     edgecolor=prot_color, color="white", alpha=0.1 )
+    ax.plot_trisurf(xp2, yp2, zp2, triangles=face_protein_2, linewidth=0.2,
+                     edgecolor=prot_color, color="white", alpha=0.1 )
+
+    ax.set_xlabel('X [$\AA$]')
+    ax.set_ylabel('Y [$\AA$]')
+    ax.set_zlabel('Z [$\AA$]')
+
+    ax.xaxis.labelpad=10
+    ax.yaxis.labelpad=10
+    ax.zaxis.labelpad=10
+
+
+    ax.view_init(elev, azim) 
+    fig.savefig(file_name+'.'+file_ext, bbox_inches='tight', pad_inches=0.1,
+                format=file_ext)
+
+
+
+
