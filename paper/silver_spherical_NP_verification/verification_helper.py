72b103dc72f920fd6dd5e51db3b6b3ccbb511ac4
Naty Clementi <natyclementi@gmail.com>
2018-06-07 17:00:38 -0400

    Modify script to generate paper fig

--- paper/silver_spherical_NP_verification/verification_helper.py
+++ paper/silver_spherical_NP_verification/verification_helper.py
@@ -31,35 +31,57 @@ def Cext_analytical(radius, wavelength, diel_out, diel_in):
     return Cext_an
 
 def plot_cext_wave(lamb, cext, cext_an, ylim_s, ylim_e, xpoints, title=None, 
-                    file_name=None, file_ext=None):
+                    file_name=None, file_ext=None, paper=False):
+
+    if paper:
+        file_ext = 'pdf'
+        pyplot.switch_backend('agg')
+        fig = pyplot.figure(figsize=(3, 2))
+        ms = 5
+        lw = 1
+        fs = 10
+        hl = 0.1
+        fsl = 9
+    else:
+        pyplot.figure(figsize=(6, 4))
+        ms = 7
+        lw = 2
+        fs = 12
+        hl = 0.5
+        fsl = 12
 
     rcParams['font.family'] = 'serif'
-    rcParams['font.size'] = 14
+    rcParams['font.size'] = 12
     rcParams['xtick.top'] = True
     rcParams['ytick.right'] = True
-    rcParams['axes.linewidth'] = 2
+    rcParams['axes.linewidth'] = 1
     
-    pyplot.figure(figsize=(9,6))
 
-    pyplot.plot(lamb, cext, ls='', marker='o', color='0.4', mew=1.5, mfc='w', ms=7, label='PyGBe')
-    pyplot.plot(lamb, cext_an, ls='--', marker='None',  c='k', lw=1.5, label='Analytical')
+    pyplot.plot(lamb, cext, ls='', marker='o', color='0.5', mew=1, mfc='w', ms=ms, label='PyGBe')
+    pyplot.plot(lamb, cext_an, ls='--', marker='None',  c='k', lw=lw, label='Analytical')
 
 
     pyplot.xlabel('Wavelength [nm]')
-    pyplot.ylabel('Extinction cross section [$nm^2$]')
+    pyplot.ylabel(' $C_{ext}$ [$nm^2$]')
     pyplot.xlim(min(lamb), max(lamb))
     pyplot.ylim(ylim_s, ylim_e)
 
     pyplot.xticks(numpy.linspace(min(lamb), max(lamb), xpoints), rotation=25)
-    pyplot.tick_params(axis='both', length=8, width=1, direction='in')
+    pyplot.yticks(numpy.linspace(0, 4000, 9))
+    pyplot.tick_params(axis='both', length=5, width=0.8, direction='in')
+
+
     
     if title:
         pyplot.title(title)
     
-    pyplot.legend(loc='best')
+    pyplot.legend(loc='upper right', fontsize=fsl, numpoints=1, handlelength=hl).get_frame().set_lw(0.2)
     pyplot.grid(linestyle=':')
 
     if file_name and file_ext:
         pyplot.savefig(file_name+'.'+file_ext, format=file_ext, dpi=80, 
                         bbox_inches='tight', pad_inches=0.1)
+    if paper :
+        pyplot.close(fig)
+
 
